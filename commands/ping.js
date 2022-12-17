dddb908edaaa8430469713b1d59f2b37547cbc79
Dalia Sofía Magallón Páramo <magallon.paramo@gmail.com>
2022-12-16 18:27:14 -0600

    Fix ping

--- commands/ping.js
+++ commands/ping.js
@@ -6,13 +6,6 @@ module.exports = {
         .setName('ping')
         .setDescription('Responde con un pong'),
     async execute(interaction){
-        const result = await interaction.reply('Pong!');
-        const timeInteraction = interaction.createdTimestamp
-        const timeResponse = convert(result.id)
-        const resultValue = Math.abs(timeInteraction-timeResponse);
-
-        console.log(timeInteraction, timeResponse)
-
-        interaction.editReply(`Pong! Ping was ${resultValue}`);
+        return interaction.reply('Pong!');
     },
 }
